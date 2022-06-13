5a6c3a7d8e7f68bf927100fa27845a45b6b51aa3
hzp <hzp.3020@gmail.com>
2022-06-13 15:50:56 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -113,3 +113,16 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// class StatelessContainer extends StatelessWidget {
+//   final Color color = RandomColor().randomColor();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 100,
+//       height: 100,
+//       color: color,
+//     );
+//   }
+// }
